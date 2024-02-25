1aded5d8a9211300c8471d717f69c11b7c846be6
Swostika Shrestha <shrestha.swosti1271@gmail.com>
2024-02-25 16:05:58 +0545

    Product is added

--- hello.py
+++ hello.py
@@ -2,5 +2,8 @@ num1 = 1.5
 num2 = 6.3
 
 sum = num1 + num2
+product=num1*num2
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The product of {0} and {1} is {2}'.format(num1, num2, product))
+
