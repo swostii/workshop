2beea42b91c7b65d5d906fe2ba5983fd89382891
Swostika Shrestha <shrestha.swosti1271@gmail.com>
2024-02-25 16:15:13 +0545

    Changes made in printing result

--- hello.py
+++ hello.py
@@ -5,8 +5,8 @@ sum = num1 + num2
 product=num1*num2
 difference= num2 -num1
 
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-print('The product of {0} and {1} is {3}'.format(num1, num2, product))
-print('The difference of {0} and {1} is {4}'.format(num1, num2, difference))
+print('The sum is',sum)
+print('The product is', product)
+print('The difference is', difference)
 
 
