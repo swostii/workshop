72751be0138bfd2449a7227a21bd69bb08c11b3a
Swostika Shrestha <shrestha.swosti1271@gmail.com>
2024-02-25 16:09:42 +0545

    Difference is added

--- hello.py
+++ hello.py
@@ -3,7 +3,10 @@ num2 = 6.3
 
 sum = num1 + num2
 product=num1*num2
+difference= num2 -num1
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-print('The product of {0} and {1} is {2}'.format(num1, num2, product))
+print('The product of {0} and {1} is {3}'.format(num1, num2, product))
+print('The difference of {0} and {1} is {4}'.format(num1, num2, difference))
+
 
